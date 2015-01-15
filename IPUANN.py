68216d2096f1b19bc74c75d2fe383872a82c4d48
Walter Sagehorn <w.sagehorn@gmail.com>
2015-01-14 21:42:58 -0600

    Added main file

---
+++ IPUANN.py
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Name:        IPUANN - Image Processing Using Artificial Neural Networks
+# Purpose:
+#
+# Author:      Walter Sagehorn
+#
+#-------------------------------------------------------------------------------
+
+
+"""
+imports
+
+class hidden layer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
